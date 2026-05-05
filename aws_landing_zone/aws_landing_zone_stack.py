03594bc9faff35949735556bd0892332ce87c240
rwvtube <rwvtube@gmail.com>
2026-05-05 11:35:21 +0530

    feat: first cdk deployment with billing alarm

--- aws_landing_zone/aws_landing_zone_stack.py
+++ aws_landing_zone/aws_landing_zone_stack.py
@@ -1,19 +1,21 @@
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_cloudwatch as cloudwatch,
 )
 from constructs import Construct
 
 class AwsLandingZoneStack(Stack):
-
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "AwsLandingZoneQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        # 1% Strategy: First resource is ALWAYS a billing alarm
+        cloudwatch.Alarm(self, "BillingAlarm",
+            comparison_operator=cloudwatch.ComparisonOperator.GREATER_THAN_THRESHOLD,
+            threshold=10,
+            evaluation_periods=1,
+            metric=cloudwatch.Metric(
+                namespace="AWS/Billing",
+                metric_name="EstimatedCharges",
+                dimensions_map={"Currency": "USD"}
+            )
+        )
