71293c4965911a078af65fbc5c1db208aafc23ac
rwvtube <rwvtube@gmail.com>
2026-05-07 09:49:25 +0530

    Logarchievestack-2

--- network_stacks/network_stack.py
+++ network_stacks/network_stack.py
@@ -1,2 +1,9 @@
-class NetworkStack:
-    pass
+from aws_cdk import (
+    Stack,
+)
+from constructs import Construct
+
+class NetworkStack(Stack):
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+        # We will add VPC logic here later
--- security_stacks/log_archive_stack.py
+++ security_stacks/log_archive_stack.py
@@ -1,2 +1,10 @@
-class LogArchiveStack:
-    pass
+from aws_cdk import (
+    Stack,
+)
+from constructs import Construct
+
+
+class LogArchiveStack(Stack):
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+        # We will add S3 Bucket logic here later
